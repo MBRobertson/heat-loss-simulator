64ff18f07716d355bbbd5063b695a8593289ee7f
Michael Robertson <mbr.robertson@gmail.com>
2019-09-30 17:36:57 +1300

    Calculate temperature as a linear interpolation of forecasts

--- weather.py
+++ weather.py
@@ -12,16 +12,24 @@ class WeatherData:
 
     def at_time(self, d_time):
         """
-		Get the current temperature at a given time
-		:param d_time: datetime object to get weather for
-		:return: A number represent temperature in celsius
-		"""
+        Get the current temperature at a given time
+        :param d_time: datetime object to get weather for
+        :return: A number represent temperature in celsius
+        """
         # Check for the range if fits between
         for i in range(len(self.data) - 1):
             day_data = self.data[i]
             next_day_data = self.data[i + 1]
             if day_data[0] < d_time < next_day_data[0]:
-                return day_data[1]
+                # Calculate temperature linearly between to two points
+                temp_a = day_data[1]
+                temp_b = next_day_data[1]
+                temp_delta = temp_b - temp_a
+                a_seconds = (day_data[0] - d_time).total_seconds()
+                b_seconds = (d_time - next_day_data[0]).total_seconds()
+
+                return temp_a + (temp_delta * (a_seconds / (a_seconds + b_seconds)))
+
         # Check if date is beyond last entry
         if len(self.data) > 0 and self.data[-1][0] < d_time:
             return self.data[-1][1]
@@ -31,10 +39,10 @@ class WeatherData:
 
 def from_openweathermap(town='hamilton'):
     """
-	Fetch 5 day 3 hourly weather forecast from open weather map
-	:param town: The location to get weather data for
-	:return: Weather data object to query temperature
-	"""
+    Fetch 5 day 3 hourly weather forecast from open weather map
+    :param town: The location to get weather data for
+    :return: Weather data object to query temperature
+    """
     url = f'https://api.openweathermap.org/data/2.5/forecast?q={town},nz&mode=json&appid={OPEN_WEATHER_MAP_KEY}&units=metric'
     response = requests.get(url)
 
@@ -50,14 +58,14 @@ def from_openweathermap(town='hamilton'):
 
 def from_metservice(town='hamilton'):
     """
-	Get the 10 day weather forcast for the specified town if it exists
-	Default town is hamilton, list of towns is on metservice website
-	# Where forecasts are taken from: https://www.metservice.com/towns-cities/hamilton#!/ten-day
-	:param town: Town to get
-	:return: A list of the next days weather
-	"""
+    Get the 10 day weather forcast for the specified town if it exists
+    Default town is hamilton, list of towns is on metservice website
+    # Where forecasts are taken from: https://www.metservice.com/towns-cities/hamilton#!/ten-day
+    :param town: Town to get
+    :return: A list of the next days weather
+    """
 
-    # Full url for the 10 day forecase from metservice
+    # Full url for the 10 day forecast from metservice
     url = 'https://www.metservice.com/publicData/localForecast' + str(town).lower()
 
     response = requests.get(url)
