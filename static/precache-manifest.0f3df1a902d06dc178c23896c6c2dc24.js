bbd4f297258d844098df6e3b19c7f46365f5f9e3
Michael Robertson <mbr.robertson@gmail.com>
2019-10-12 16:01:26 +1300

    Added built client  - Bit dirty, but don't have time to set up nice build processes

---
+++ static/precache-manifest.0f3df1a902d06dc178c23896c6c2dc24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8048067caf067945bee212209f63191",
+    "url": "/index.html"
+  },
+  {
+    "revision": "621821f0464358dc6071",
+    "url": "/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "d4137ba4ecf45edb98e7",
+    "url": "/static/css/main.5950be92.chunk.css"
+  },
+  {
+    "revision": "621821f0464358dc6071",
+    "url": "/static/js/2.df895baf.chunk.js"
+  },
+  {
+    "revision": "d4137ba4ecf45edb98e7",
+    "url": "/static/js/main.8571487e.chunk.js"
+  },
+  {
+    "revision": "532ef33148bcf816608f",
+    "url": "/static/js/runtime-main.b74d8bc4.js"
+  }
+]);
